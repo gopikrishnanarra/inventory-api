5a0c68e530b4335485b61c42a2dd79cc7feac66a
gn59762 <narragopikrishna@johndeere.com>
2019-10-03 13:38:21 -0500

    add route for user login details

---
+++ routes/userDetails.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+let response = {
+    login: false,
+    passwordMissMatch: false,
+    userExists: false
+};
+
+router.get('/', async function(req, res) {
+    const users = await axios.get('https://api.mlab.com/api/1/databases/users/collections/users-list?apiKey=kIOuLscCmhbeSOoBEtJUYPV6vy1TMIaQ');
+    const { id, password } = req.query;
+    const userExists = users.data.filter((user) => {
+        return user.userId === id
+    });
+    if (userExists) {
+        users.data.forEach((user) => {
+            if (user.userId === id) {
+                if (user.password === password) {
+                    response.login = true;
+                    response.userExists = true;
+                    response.passwordMissMatch = false;
+                } else {
+                    response.login = false;
+                    response.passwordMissMatch = true;
+                    response.userExists = true;
+                }
+            }
+        });
+    } else if (!userExists) {
+        response.login = false;
+        response.userExists = false;
+        response.passwordMissMatch = false;
+    }
+    res.send(response);
+});
+
+module.exports = router;
