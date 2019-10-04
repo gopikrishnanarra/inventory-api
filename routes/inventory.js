2f577181cb2924529fd62c6344b672b2b1a391a0
gn59762 <narragopikrishna@johndeere.com>
2019-10-04 14:58:30 -0500

    refactor

--- routes/inventory.js
+++ routes/inventory.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.get('/', async function(req, res) {
     const inventory = await axios.get("https://api.mlab.com/api/1/databases/inventory/collections/inventory?apiKey=kIOuLscCmhbeSOoBEtJUYPV6vy1TMIaQ")
-    await axios.post("http://localhost:9000/user/create", {"userId":"admin"});
     res.send(inventory.data);
 });
 
