21ffc8e7aa56ad6e5ba1974c5a631acb3c345248
gopikrishnanarra <gopiknarra@gmail.com>
2019-10-05 01:19:45 -0500

    add path for save inventory

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+INVENTORY_URL = "https://api.mlab.com/api/1/databases/inventory/collections/inventory?apiKey=kIOuLscCmhbeSOoBEtJUYPV6vy1TMIaQ";
 
 router.get('/', async function(req, res) {
-    const inventory = await axios.get("https://api.mlab.com/api/1/databases/inventory/collections/inventory?apiKey=kIOuLscCmhbeSOoBEtJUYPV6vy1TMIaQ")
+    const inventory = await axios.get(INVENTORY_URL);
     res.send(inventory.data);
+});
+router.post('/addItem', async function(req, res) {
+    await axios.post(INVENTORY_URL, req.body);
+    res.send({})
 });
 
 module.exports = router;
